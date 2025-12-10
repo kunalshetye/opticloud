bde67e51b9d8cf4f05fbe9bedac5b53bdf6ae5c6
Kunal Shetye <kunal.shetye@gmail.com>
2025-12-10 11:22:24 +0100

    added support for .env files

--- bin/dev.js
+++ bin/dev.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S node --loader ts-node/esm --no-warnings
 
+// Load environment variables from .env file
+import 'dotenv/config'
+
 async function main() {
   const {execute} = await import('@oclif/core')
   await execute({development: true, dir: import.meta.url})
--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// Load environment variables from .env file
+require('dotenv').config()
+
 const oclif = require('@oclif/core')
 
 oclif.run().then(require('@oclif/core/handle'), require('@oclif/core/handle'))
